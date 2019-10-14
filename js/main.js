5513538d73789513973df520ec40dfb8e60801d5
irina.boiko <irinaboiko95@gmail.com>
2019-10-15 00:06:15 +0300

    mobile

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-promo').slick({
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        speed: 1500,
+        arrows: false,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+    });
+});
