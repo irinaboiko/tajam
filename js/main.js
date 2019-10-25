aa74df7c4249dfd6048ca75d6f19d42381387ba9
irina.boiko <irinaboiko95@gmail.com>
2019-10-25 23:26:20 +0300

    add gallery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
     $('.slider-promo').slick({
         dots: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         speed: 1500,
         arrows: false,
         pauseOnHover: true,
         pauseOnDotsHover: true,
     });
+    $('.slider-text-review').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-clients',
+        draggable: false,
+      });
+      $('.slider-clients').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-text-review',
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '60px',
+        focusOnSelect: true,
+      });
 });
