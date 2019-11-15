2e1217993d389b38726c688a21fed3cbc72eea2f
irina.boiko <irinaboiko95@gmail.com>
2019-11-15 22:32:16 +0300

    loader

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 /*loader*/
 
 
-$(window).on('loader-inner', function () {
-  $preloader = $('.loader'),
-    $loader = $preloader.find('.loader-inner');
-  $loader.fadeOut();
-  $preloader.delay(350).fadeOut('slow');
-});
 
 
 /*scroll menu*/
